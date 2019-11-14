281d44f51b7e00fe5596d725809dbda6c6a970fa
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-14 14:15:10 +0000

    make header bigger with coral font color

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header = ({ children }) => {
   return (
-    <header>
+    <header className={styles["header"]}>
       <h1>{children}</h1>
     </header>
   );
