b6d5abc363d5afd146a15550ea498fb323db5cce
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-14 14:18:03 +0000

    /jobs takes query/params

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,6 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import withContext from "../Consumer";
+import axios from "axios";
 
 import Autocomplete from "./Autocomplete";
-import OptionsList from "./OptionsList";
 
-const Form =
+const Form = ({ context }) => {
+  const { setJobs } = context;
+  const [userInput, setUserInput] = useState("");
+  const [options, setOptions] = useState();
+  const [showWarningMessage, setShowWarningMessage] = useState(false);
+  const [showLoadingMessage, setShowLoadingMessage] = useState(false);
+
+  const handleOnSubmit = async e => {
+    e.preventDefault();
+    const userInputIsACity = options.includes(userInput);
+
+    if (userInputIsACity) {
+      try {
+        setJobs(undefined);
+        setShowLoadingMessage(true);
+        const response = await axios.get("/jobs", {
+          params: {
+            city: userInput.toLowerCase(),
+            resultsToTake: "100"
+          }
+        });
+        const { data } = response;
+        const jobResults = data["results"];
+        setShowLoadingMessage(false);
+        setJobs(jobResults);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setUserInput("");
+      setShowWarningMessage(true);
+    }
+  };
+  useEffect(() => {
+    async function getCities() {
+      try {
+        const response = await axios.get("/cities");
+        const { data } = response;
+        setOptions(data);
+      } catch (error) {
+        throw error;
+      }
+    }
+    getCities();
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <Autocomplete
+          options={options}
+          userInput={userInput}
+          setUserInput={setUserInput}
+          setShowWarningMessage={setShowWarningMessage}
+        />
+      </form>
+      {showWarningMessage && (
+        <p>Please submit a valid city from the dropdown </p>
+      )}
+      {showLoadingMessage && (
+        <p>
+          <span aria-label="looking emoji">🧐</span> Getting jobs...
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default withContext(Form);
