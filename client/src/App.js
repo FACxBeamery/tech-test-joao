d5cd23ecbaeb5962166b9ba47319c60c21f2d5bf
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-14 14:13:53 +0000

    Provider creates context and wraps Form and JobsList components

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,29 @@
 import React from "react";
 import Header from "./components/Header";
 import SubHeader from "./components/SubHeader";
+import Footer from "./components/Footer";
 import Form from "./components/Form";
-import Autocomplete from "./components/Autocomplete";
+import JobsList from "./components/JobList";
 import styles from "./App.module.css";
+
+import Provider from "./components/Provider";
 
 function App() {
   return (
-    <div>
-      <Header>Job Search Engine</Header>
+    <div className={styles["app"]}>
+      <Header>Job Search Genie</Header>
+
       <main className={styles["main"]}>
         <SubHeader>
           Find relevant job posts in a UK city of your preference
         </SubHeader>
-        <Autocomplete
-          options={[
-            "London",
-            "Birmingham",
-            "Leeds",
-            "Sheffield",
-            "Bradford",
-            "Manchester",
-            "Liverpool",
-            "Bristol",
-            "Newcastle",
-            "Sunderland",
-            "Wolverhampton"
-          ]}
-        />
+
+        <Provider>
+          <Form />
+          <JobsList />
+        </Provider>
       </main>
+      <Footer>2019 - João Viana</Footer>
     </div>
   );
 }
