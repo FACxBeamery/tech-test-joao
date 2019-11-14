ffe86345caf04f91333088e0cb1a84eea64ac2ea
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-14 22:09:57 +0000

    jobs response variable name change

--- server/src/handlers/getJobs.test.js
+++ server/src/handlers/getJobs.test.js
@@ -4,30 +4,28 @@ const nock = require("nock");
 const supertest = require("supertest");
 const router = require("../router");
 const app = express();
-const bodyParser = require("body-parser");
-const dummyResponse = require("../utils/dummyData/dummyResponse");
+const { jobsResponse } = require("../utils/dummyData/dummyResponse");
 
 describe("GET /jobs", () => {
-  //   app.use(bodyParser());
-  app.use(router);
-  it("it shoud return status code 200 to indicate jobs were retrieved", done => {
-    nock("https://www.reed.co.uk/api/1.0")
-      .get("/search")
-      .query(true)
-      .reply(200, dummyResponse);
+    app.use(router);
+    it("it shoud return status code 200 to indicate jobs were retrieved", done => {
+        nock("https://www.reed.co.uk/api/1.0")
+            .get("/search")
+            .query(true)
+            .reply(200, jobsResponse);
 
-    supertest(app)
-      .get(`/jobs`)
-      .query({ city: "london", nrresults: "100" })
-      .expect("Content-Type", /json/)
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        } else {
-          assert.equal(res.status, "200");
-          assert.deepEqual(JSON.parse(res.text), dummyResponse);
-          done();
-        }
-      });
-  });
+        supertest(app)
+            .get(`/jobs`)
+            .query({ city: "london", nrresults: "100" })
+            .expect("Content-Type", /json/)
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                } else {
+                    assert.equal(res.status, "200");
+                    assert.deepEqual(res.body, jobsResponse);
+                    done();
+                }
+            });
+    });
 });
