8772d6a77e2d0325cc729b3ab2615d5f47a9e7d8
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-14 14:18:37 +0000

    /cities route

--- server/src/router.js
+++ server/src/router.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express();
 
 const getJobs = require("./handlers/getJobs");
+const getCities = require("./handlers/getCities");
 
 router.get("/jobs", getJobs);
+router.get("/cities", getCities);
 
 module.exports = router;
