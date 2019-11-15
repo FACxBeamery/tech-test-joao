64c0d4f4fa642ff026370396ae82d42f222a67fe
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-15 13:08:43 +0000

    renaming of query Relates to #9

--- server/src/handlers/getJobs.test.js
+++ server/src/handlers/getJobs.test.js
@@ -7,25 +7,25 @@ const app = express();
 const { jobsResponse } = require("../utils/dummyData/dummyResponse");
 
 describe("GET /jobs", () => {
-    app.use(router);
-    it("it shoud return status code 200 to indicate jobs were retrieved", done => {
-        nock("https://www.reed.co.uk/api/1.0")
-            .get("/search")
-            .query(true)
-            .reply(200, jobsResponse);
+  app.use(router);
+  it("it shoud return status code 200 to indicate jobs were retrieved", done => {
+    nock("https://www.reed.co.uk/api/1.0")
+      .get("/search")
+      .query(true)
+      .reply(200, jobsResponse);
 
-        supertest(app)
-            .get(`/jobs`)
-            .query({ city: "london", nrresults: "100" })
-            .expect("Content-Type", /json/)
-            .end((err, res) => {
-                if (err) {
-                    done(err);
-                } else {
-                    assert.equal(res.status, "200");
-                    assert.deepEqual(res.body, jobsResponse);
-                    done();
-                }
-            });
-    });
+    supertest(app)
+      .get(`/jobs`)
+      .query({ city: "london", nrOfResults: "100" })
+      .expect("Content-Type", /json/)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        } else {
+          assert.equal(res.status, "200");
+          assert.deepEqual(res.body, jobsResponse);
+          done();
+        }
+      });
+  });
 });
