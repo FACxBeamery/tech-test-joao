74327c09485be1653a1146301e0c5a793622c12e
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-15 12:38:08 +0000

    dummy jobs data

---
+++ client/src/utils/dummyJobsData.js
@@ -0,0 +1,95 @@
+const jobsData = [
+  {
+    jobId: 39373637,
+    employerId: 381223,
+    employerName: "Morgan Hunt UK Limited",
+    employerProfileId: null,
+    employerProfileName: null,
+    jobTitle: "Accountant",
+    locationName: "Leyland",
+    minimumSalary: 13.88,
+    maximumSalary: 16.0,
+    currency: "GBP",
+    expirationDate: "13/12/2019",
+    date: "15/11/2019",
+    jobDescription:
+      " Job title: Planning and Development Accountant Contract: 3 - 6 months Location: Preston Sector: Housing  I am seeking an Accountant who is immediately available for a 3 - 6 month contract  The role is responsible for completing the groups 30 year financial plans and the preparation of annual rent setting calculations. There will also... ",
+    applications: 0,
+    jobUrl: "https://www.reed.co.uk/jobs/accountant/39373637"
+  },
+  {
+    jobId: 39373627,
+    employerId: 606056,
+    employerName: "AD Finance",
+    employerProfileId: null,
+    employerProfileName: null,
+    jobTitle: "Management Accountant",
+    locationName: "Walsall",
+    minimumSalary: 35000.0,
+    maximumSalary: 40000.0,
+    currency: "GBP",
+    expirationDate: "27/12/2019",
+    date: "15/11/2019",
+    jobDescription:
+      " Alexander Daniels are currently recruiting for an exciting part qualified management accountant to work for a growing SME based in Walsall. The purpose of the role will be to ensure management accounts, budgeting and forecasting and analysis are delivered in a timely and accurate manner. The Management Accountant will also ensure robust accounting controls are in place and maintained. Key Responsibilities: ... ",
+    applications: 0,
+    jobUrl: "https://www.reed.co.uk/jobs/management-accountant/39373627"
+  },
+  {
+    jobId: 39373613,
+    employerId: 419329,
+    employerName: "Global Accounting Network",
+    employerProfileId: null,
+    employerProfileName: null,
+    jobTitle: "Interim Management Accountant",
+    locationName: "South East London",
+    minimumSalary: 250.0,
+    maximumSalary: 375.0,
+    currency: "GBP",
+    expirationDate: "27/12/2019",
+    date: "15/11/2019",
+    jobDescription:
+      " Our client, one of the UK’s most prominent fintech businesses requires an Interim Management Accountant. The business offers a variety of solutions to their diverse client base at a local and international level - as Interim Management Accountant, this is  a fantastic opportunity to join one of the fastest growing companies in a progressive sector. The Interim Management Accountant will be based in Central London and benefit from an energeti... ",
+    applications: 0,
+    jobUrl: "https://www.reed.co.uk/jobs/interim-management-accountant/39373613"
+  },
+  {
+    jobId: 39373579,
+    employerId: 472689,
+    employerName: "Page Personnel Finance",
+    employerProfileId: null,
+    employerProfileName: null,
+    jobTitle: "Assistant Divisional Accountant",
+    locationName: "Swanley",
+    minimumSalary: null,
+    maximumSalary: null,
+    currency: null,
+    expirationDate: "27/12/2019",
+    date: "15/11/2019",
+    jobDescription:
+      " A fantastic opportunity for an assistant management accountant to join this reputable construction business in North Kent. Client Details My client is a superb company to work for looking to add an assistant management accountant to their busy team for an initial period of 12 months. Description The core responsibilities include but are not limited to:   Processing... ",
+    applications: 0,
+    jobUrl:
+      "https://www.reed.co.uk/jobs/assistant-divisional-accountant/39373579"
+  },
+  {
+    jobId: 39373530,
+    employerId: 472689,
+    employerName: "Page Personnel Finance",
+    employerProfileId: null,
+    employerProfileName: null,
+    jobTitle: "Assistant Accountant",
+    locationName: "St. Albans",
+    minimumSalary: 27000.0,
+    maximumSalary: 28000.0,
+    currency: "GBP",
+    expirationDate: "22/11/2019",
+    date: "15/11/2019",
+    jobDescription:
+      " My client are currently recruiting for an Assistant Accountant to come in and take responsibility of the transactional finance procedures within their friendly, family orientated finance department. They are ideally seeking an ambitious candidate studying  AAT or with a recent degree qualification, although these are not essentials. Client Details A market leading construction company. Family run since their in... ",
+    applications: 0,
+    jobUrl: "https://www.reed.co.uk/jobs/assistant-accountant/39373530"
+  }
+];
+
+export default jobsData;
