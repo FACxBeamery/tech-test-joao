6530e1111286691811fc9e98704e93b7357755ed
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-15 00:27:11 +0000

    Provider documentation

--- client/src/components/Provider.js
+++ client/src/components/Provider.js
@@ -2,13 +2,23 @@ import React, { useState, createContext } from "react";
 
 export const AppContext = createContext();
 
+/**
+ * HOC that ensures its consuming components subscribe to context changes.
+ */
+
 const Provider = ({ children }) => {
-  const [jobs, setJobs] = useState(undefined);
-  return (
-    <AppContext.Provider value={{ jobs, setJobs }}>
-      {children}
-    </AppContext.Provider>
-  );
+    const [jobs, setJobs] = useState();
+
+    return (
+        <AppContext.Provider
+            value={{
+                jobs,
+                setJobs
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
 };
 
 export default Provider;
